4f690d35b36ad3c909c03220240c0c60f371f324
tylerbattler <tychriste@gmail.com>
2019-04-16 18:20:02 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Router>
+          <div>
+            <h1>Tyler Christensens Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route path="/portfolio/:slug" component={PortfolioDetail} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
